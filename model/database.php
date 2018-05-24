1430a5d7ed3695433397bc88dfcf8d5f8eada834
JohnCR92 <johncuasapud@gmail.com>
2018-05-23 22:07:49 -0500

    cambio

--- model/database.php
+++ model/database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function Conectar()
     {
-        $pdo = new PDO('mysql:host=servidorjohn1.mysql.database.azure.com;dbname=bddjohn;charset=utf8', 'bddjohn@servidorjohn1', 'JohnHCR92');
+        $pdo = new PDO('mysql:host=bddjohn.mysql.database.azure.com;dbname=bddjohn;charset=utf8', 'johncr@bddjohn', 'JohnHCR92');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
     }
